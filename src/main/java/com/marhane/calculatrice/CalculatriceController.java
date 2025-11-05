9df5878a2825c5c1bd70341e1c9e7a1921954c4c
abih-774 <hibamarhane121@gmail.com.com>
2025-11-05 17:07:52 +0100

    Ajouter le squelette Spring Boot change

--- src/main/java/com/marhane/calculatrice/CalculatriceController.java
+++ src/main/java/com/marhane/calculatrice/CalculatriceController.java
@@ -1,19 +1,15 @@
 package com.marhane.calculatrice;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 class CalculatriceController {
-
     @Autowired
     private Calculatrice calculatrice;
-
     @RequestMapping("/sum")
-    public String sum(@RequestParam("a") Integer a,
-                      @RequestParam("b") Integer b) {
+    String sum(@RequestParam("a") Integer a,
+               @RequestParam("b") Integer b) {
         return String.valueOf(calculatrice.sum(a, b));
     }
 }
